9e90c55503aabc815db009e6fbbf75d5b9c51a92
Anton Stakhouski <antonstakhouski@gmail.com>
2017-03-14 18:03:43 +0300

    first lab is working fine

--- src/main.py
+++ src/main.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cmath
 import math
+import matplotlib.gridspec as gridspec
+#  import ipdb
 
 
 class Calculator:
@@ -15,6 +17,7 @@ class Calculator:
         self.x_array = np.arange(self.start, self.end, self.step)
         self.y_array = self.y(self.x_array)
         self.dft_array = list()
+        self.count = 0
 
     def y(self, x):
         return np.cos(2 * x) + np.sin(5 * x)
@@ -28,6 +31,49 @@ class Calculator:
             m += 1
         return (1 / self.n) * c
 
+    def fft_dit(self, a, direct):
+        if len(a) == 1:
+            return a
+        a_even = list()
+        a_odd = list()
+        i = 0
+        while (i < len(a)):
+            if i % 2 == 0:
+                a_even.append(a[i])  # четный
+            else:
+                a_odd.append(a[i])
+            i += 1
+        b_even = self.fft_dit(a_even, direct)
+        b_odd = self.fft_dit(a_odd, direct)
+
+        #  print(self.count)
+        self.count += 1
+
+        #  print(b_even)
+        #  print(b_odd)
+        argument = 2 * cmath.pi / len(a)
+        wn = cmath.cos(argument) + direct * cmath.sqrt(-1) * cmath.sin(argument)
+
+        w = np.complex(1)
+        #  print(w)
+
+        y = [np.complex(0)] * len(a)
+        j = 0
+        while(j < len(a) // 2):
+            y[j] = b_even[j] + w * b_odd[j]
+            #  print(y[j])
+            #  self.t(y[j])
+            y[j + len(a) // 2] = b_even[j] - w * b_odd[j]
+            #  print(y[j] + len(a) // 2)
+            #  self.t(y[j] + len(a) // 2)
+            w *= wn
+            j += 1
+        #  print(y)
+        return y
+
+    def t(self, x):
+        print(type(x))
+
     def idft(self, m):
         k = 0
         x = 0
@@ -40,39 +86,73 @@ class Calculator:
     def draw_signal(self, fignum):
         plt.subplot(fignum)
         plt.title("Default signal")
-        plt.plot(self.x_array, self.y_array)
+        abs_y = np.absolute(self.y_array)
+        plt.vlines(self.x_array, 0, abs_y)
 
     def draw_dft(self, fignum):
         plt.subplot(fignum)
         plt.title("Discrete Fourier Transform")
-        real_array = list()
         self.dft_array.clear()
-        for num in self.x_array:
-            cnum = self.dft(num)
-            real_array.append(cnum.real)
+        i = 0
+        while i < self.n:
+            cnum = self.dft(i)
             self.dft_array.append(cnum)
-        arr = np.array(real_array)
-        plt.plot(self.x_array, arr)
+            i += 1
+        arr = np.array(self.dft_array)
+        abs_y = np.absolute(arr)
+        plt.vlines(self.x_array, 0, abs_y)
 
     def draw_idft(self, fignum):
         plt.subplot(fignum)
         plt.title("Inverse Discrete Fourier Transform")
-        real_array = list()
-        for num in self.x_array:
-            cnum = self.idft(num)
-            real_array.append(cnum.real)
-        arr = np.array(real_array)
-        plt.plot(self.x_array, arr)
+        idft_array = list()
+        i = 0
+        while i < self.n:
+            idft_array.append(self.idft(i))
+            i += 1
+        arr = np.array(idft_array)
+        abs_y = np.absolute(arr)
+        plt.vlines(self.x_array, 0, abs_y)
+
+    def draw_fft(self, fignum):
+        plt.subplot(fignum)
+        plt.title("Fast Fourier Transform")
+        fft_array = self.fft_dit(self.y_array, -1)
+        self.dft_array = fft_array
+        #  print(type(fft_array[0]))
+        print(fft_array)
+
+        abs_y = list()
+        for num in fft_array:
+            abs_y.append(np.absolute(num.real))
+        plt.vlines(self.x_array, 0, abs_y)
+
+    def draw_fft_inv(self, fignum):
+        plt.subplot(fignum)
+        plt.title("Inverse Fast Fourier Transform")
+        fft_array = self.fft_dit(self.dft_array, 1)
+        arr = np.array(fft_array)
+
+        fft_real = list()
+        abs_y = list()
+        for num in arr:
+            fft_real.append(num)
+            abs_y.append(np.absolute(num))
+        plt.vlines(self.x_array, 0, abs_y)
 
     def draw(self):
-        plt.figure(1)
-        self.draw_signal(511)
-        self.draw_dft(512)
-        self.draw_idft(513)
+        gs = gridspec.GridSpec(2, 3)
+        self.draw_signal(gs[0, 0])
+        self.draw_dft(gs[0, 1])
+        self.draw_idft(gs[1, 1])
 
+        self.draw_fft(gs[0, 2])
+        self.draw_fft_inv(gs[1, 2])
         plt.show()
 
 
 if __name__ == '__main__':
     calc = Calculator()
+    #  ipdb.set_trace()
+    #  calc.fft_dit(calc.x_array, calc.n, 1)
     calc.draw()
